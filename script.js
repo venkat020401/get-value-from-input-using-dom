a3650fc5063adf45e961f54ecef64562ca0f87a6
Venkatajalapathy P <113085054+venkat020401@users.noreply.github.com>
2022-10-12 12:32:39 +0530

    get-value-from-input-using-dom

---
+++ script.js
@@ -0,0 +1,9 @@
+function foo(){
+var first=document.getElementById("first name").value;
+var middle=document.getElementById("mid name").value;
+var last=document.getElementById("last name").value;
+var email=document.getElementById("email").value;
+var pass=document.getElementById("pass").value;
+console.log(first,middle,last,email,pass);
+
+}
